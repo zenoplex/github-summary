bf7228f0898b90b370c470d98a3da670e783cf72
Gen <zenoplex@gmail.com>
2016-06-10 21:25:48 +0900

    chore: modify .eslintrc to javascript

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser:  'babel-eslint',
+  extends: 'airbnb-base',
+  env:     {
+    mocha: true
+  },
+  rules:   {
+    'key-spacing': 0,
+    'no-console':  0,
+    'camelcase':   0
+  }
+};
