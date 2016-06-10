882e744a546438f436c0957e7e68a62c01c4c6d6
Gen Ichino <zenoplex@gmail.com>
2016-06-10 16:27:38 +0900

    fix: from, to properties not being passed to date utility function

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,11 +1,11 @@
-export function getStartOfDay(day) {
-  const date = (day instanceof Date) ? day : new Date();
+export function getStartOfDay(day = new Date()) {
+  const date = (day instanceof Date) ? day : new Date(day);
   date.setHours(0, 0, 0, 0);
   return date;
 }
 
-export function getEndOfDay(day) {
-  const date = (day instanceof Date) ? day : new Date();
+export function getEndOfDay(day = new Date()) {
+  const date = (day instanceof Date) ? day : new Date(day);
   date.setHours(23, 59, 59, 999);
   return date;
 }
