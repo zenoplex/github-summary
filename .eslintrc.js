27a41d93e6361dca31733c4dfc22dbac749af04d
Gen <zenoplex@gmail.com>
2016-06-20 00:41:11 +0900

    chore: add packages and configs required to start flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
   parser:  'babel-eslint',
   extends: 'airbnb-base',
+  plugins: [
+    'flow-vars',
+  ],
   env:     {
     mocha: true
   },
   rules:   {
     'key-spacing': 0,
     'no-console':  0,
-    'camelcase':   0
+    'camelcase':   0,
+    'flow-vars/define-flow-type': 1,
+    'flow-vars/use-flow-type': 1,
   }
 };
